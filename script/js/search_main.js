7cc7568573c85b0c3f9ade21b7bdf7e69d3ea9e5
3asyice <guilhermeabreu294@gmail.com>
2025-07-23 18:26:27 -0300

    update search_main.js

--- script/js/search_main.js
+++ script/js/search_main.js
@@ -1,7 +1,7 @@
 const links = [
 { url: "../script/article/001.html", text: "Carlos Henrique Teixeira da Silva" },
 { url: "../script/article/002.html", text: "Militar Expulso" },
-{ url: "../script/article/002.html", text: "Família Inteira Morre em Acidente" },
+{ url: "../script/article/003.html", text: "Família Inteira Morre em Acidente" },
 { url: "../script/article/004.html", text: "Brasileiro Morre Afogado ao Salvar Parente" },
 { url: "../script/article/005.html", text: "Navio RMS - Titanic afunda no oceano" },
 { url: "../script/article/006.html", text: "Fim da Primeira Guerra Mundial" }
