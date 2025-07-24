3c4e5c11274bf16936e44468d81e76764325e51b
3asyice <guilhermeabreu294@gmail.com>
2025-07-24 13:04:15 -0300

    update search_main.js

--- script/js/search_main.js
+++ script/js/search_main.js
@@ -4,6 +4,7 @@ const links = [
 { url: "script/article/003.html", text: "Família Inteira Morre em Acidente" },
 { url: "script/article/004.html", text: "Brasileiro Morre Afogado ao Salvar Parente"},
 { url: "script/article/005.html", text: "Navio RMS - Titanic afunda no oceano" },
+{ url: "script/article/007.html", text: "Tragédia: Boate Kiss" },
 { url: "script/article/006.html", text: "Fim da Primeira Guerra Mundial" }
 
 
